3f381b1bb1cc1925219228529a37d93819c5416c
OmarHariry <elhariryomar2@gmail.com>
2024-10-01 12:53:06 +0300

    #6 -- Creating DB using Migration

---
+++ App/Migrations/20241001094342_InitialCreate.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace App.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BookStatuses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookStatuses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Copies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BookId = table.Column<int>(type: "int", nullable: false),
+                    StatusId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Copies", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Copies_BookStatuses_StatusId",
+                        column: x => x.StatusId,
+                        principalTable: "BookStatuses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Copies_Books_BookId",
+                        column: x => x.BookId,
+                        principalTable: "Books",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BorrowingRecords",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CopyId = table.Column<int>(type: "int", nullable: false),
+                    StudentId = table.Column<int>(type: "int", nullable: false),
+                    BorrowDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    ExpectedReturnDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    ActualReturnDate = table.Column<DateOnly>(type: "date", nullable: true),
+                    StatusId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BorrowingRecords", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BorrowingRecords_BookStatuses_StatusId",
+                        column: x => x.StatusId,
+                        principalTable: "BookStatuses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BorrowingRecords_Copies_CopyId",
+                        column: x => x.CopyId,
+                        principalTable: "Copies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                    table.ForeignKey(
+                        name: "FK_BorrowingRecords_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "BookStatuses",
+                columns: new[] { "Id", "Status" },
+                values: new object[,]
+                {
+                    { 1, "Good" },
+                    { 2, "Borrowed" },
+                    { 3, "Damaged" },
+                    { 4, "Lost" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Clean Code" },
+                    { 2, "Algorithms" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "Email", "Name", "Phone" },
+                values: new object[,]
+                {
+                    { 1, "Ali@enozom.com", "Ali", "01222224400" },
+                    { 2, "mohamed@enozom.com", "Mohamed", "0111155000" },
+                    { 3, "ahmed@enozom.com", "Ahmed", "0155553311" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Copies",
+                columns: new[] { "Id", "BookId", "StatusId" },
+                values: new object[,]
+                {
+                    { 1, 1, 1 },
+                    { 2, 2, 1 },
+                    { 3, 1, 1 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BorrowingRecords_CopyId",
+                table: "BorrowingRecords",
+                column: "CopyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BorrowingRecords_StatusId",
+                table: "BorrowingRecords",
+                column: "StatusId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BorrowingRecords_StudentId",
+                table: "BorrowingRecords",
+                column: "StudentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Copies_BookId",
+                table: "Copies",
+                column: "BookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Copies_StatusId",
+                table: "Copies",
+                column: "StatusId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BorrowingRecords");
+
+            migrationBuilder.DropTable(
+                name: "Copies");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropTable(
+                name: "BookStatuses");
+
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
